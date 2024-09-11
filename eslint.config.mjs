f5e745ae30b0f5fcb35ec35009196a26a3217757
Tareun3406 <tareun3406@gmail.com>
2024-09-11 22:56:41 +0900

    chore: eslint rule 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,5 +10,10 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
 ];
