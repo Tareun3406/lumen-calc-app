36774950e40fdcb63bff90335f986b533718187e
Tareun3406 <tareun3406@gmail.com>
2024-10-27 11:15:21 +0900

    release: 1.0.3

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -17,7 +17,7 @@ import { StaleWhileRevalidate } from "workbox-strategies";
 declare const self: ServiceWorkerGlobalScope;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const SERVICE_WORKER_VERSION = '1.0.2'
+const SERVICE_WORKER_VERSION = '1.0.3'
 console.log(SERVICE_WORKER_VERSION);
 
 clientsClaim();
